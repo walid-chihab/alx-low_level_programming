7b40099e13d89e68d95d02e6e757287b17c2f319
walid-chihab <its.n.chihab@gmail.com>
2023-09-10 13:39:12 -0700

    l

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *  * main - prints with write function
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
+
+int main(void)
+{
+     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+     return (1);
+}
