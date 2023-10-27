e57fae84dbc65c3c7c539d51a46703cddb416575
walid-chihab <its.n.chihab@gmail.com>
2023-10-27 05:31:08 -0700

    wake

--- 0x14-bit_manipulation/0-binary_uint.c
+++ 0x14-bit_manipulation/0-binary_uint.c
@@ -8,22 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int a;
-	unsigned int num;
+        int a;
+        unsigned int num;
 
-	num = 0;
-	if (!b)
-		return (0);
-	for (a = 0; b[a] != '\0'; a++)
-	{
-		if (b[a] != '0' && b[a] != '1')
-			return (0);
-	}
-	for (a = 0; b[a] != '\0'; a++)
-	{
-		num <<= 1;
-		if (b[a] == '1')
-			num += 1;
-	}
-	return (num);
+        num = 0;
+        if (!b)
+                return (0);
+        for (a = 0; b[a] != '\0'; a++)
+        {
+                if (b[a] != '0' && b[a] != '1')
+                        return (0);
+        }
+        for (a = 0; b[a] != '\0'; a++)
+        {
+                num <<= 1;
+                if (b[a] == '1')
+                        num += 1;
+        }
+        return (num);
 }
